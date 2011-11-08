8287e8c4a4d0c2e9aef9192d6b137e13475866cb
student <student@WebArchLinuxBase1.localdomain>
2011-11-08 17:25:40 +0000

    Added src directory

---
+++ src/file1.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Martin Luther King Speech - I Have a Dream</title>
+</head>
+
+<body align = center>
+<div id = "container">
+	<div id = "header">
+	<p>
+	<h1>Martin Luther King Speech - I Have a Dream</h1>
+	<hr />
+	<h2>Steps of the Lincoln Memorial</h2>
+	<br />
+	<h3>Washington D.C.</h3>
+	
+	<p>
+
+	August 28, 1963
+	<p>
+	</div>
+	<div id = "content">
+	<p>Five score years ago, a great American, in whose symbolic shadow we stand signed the Emancipation 		Proclamation.</p> 	<p>This momentous decree came as a great beacon light of hope to millions of Negro 	slaves 	who had been 	seared in the</p> 	<p>flames of withering injustice. It came as a joyous daybreak to 	end the 	long night of captivity. But one 	hundred </p>	<p>years later, we must face the tragic fact that 	the Negro 	is still not free.</p>
+	</div>
+
+	<div id = "footer">
+	</div>
+</div>
+
+</body>
+</html>
